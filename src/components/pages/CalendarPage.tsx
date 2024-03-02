7a3d7970defd17fe4e670150b3d461536ee4c8ab
wsigma21 <wallstlbl@gmail.com>
2024-03-02 12:22:30 +0900

    refactor: カレンダーページをheaderとbodyのcomponentに切り出し

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,17 +1,11 @@
-import { eachWeekOfInterval, endOfMonth, getMonth, startOfMonth, eachDayOfInterval, endOfWeek, getDate, isToday, isSameMonth } from "date-fns";
-import { DAYS_LIST } from "../../constants/calendar";
+import { eachWeekOfInterval, endOfMonth, getMonth, startOfMonth, eachDayOfInterval, endOfWeek } from "date-fns";
 import { useEffect, useState } from "react";
+import { CalendarHeader } from "../organisms/CalendarHeader";
+import { CalendarBody } from "../organisms/CalendarBody";
 
 export const CalendarPage = () => {
   const today = new Date()
   const [dateList, setDateList] = useState<Date[][]>([]);
-
-  const dateColor = (targetDate: Date, currentDate: Date): string => {
-    if (isToday(targetDate)) return "bg-lime-800 text-white rounded-full";
-    return isSameMonth(targetDate, currentDate)
-      ? "text-black"
-      : "text-gray-300";
-  };
 
   useEffect(() => {
     const monthOfSundayList = eachWeekOfInterval({
@@ -31,36 +25,8 @@ export const CalendarPage = () => {
     <>
       <h1 className="font-bold text-3xl mb-5">{`${(getMonth(today)) + 1}月`}</h1>
       <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
-        <thead>
-          <tr className="bg-lime-800 text-white rounded-tl-lg rounded-tr-lg py-10">
-            {DAYS_LIST.map((day) => (
-              <th key={day} className="text-center text-xl py-3">
-                {day}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {dateList.map((oneWeek) => (
-            <tr key={`week-${getDate(oneWeek[0])}`} className="mx-10">
-              {oneWeek.map((oneDate) => (
-                <td
-                  key={`day-${getDate(oneDate)}`}
-                  className="bg-white h-[10vh] border-2 border-solid border-lime-800"
-                >
-                  <span
-                    className={`inline-block w-[20px] leading-[20px] text-center ${dateColor(
-                      oneDate, 
-                      today
-                    )}`}
-                  >
-                    {getDate(oneDate)}
-                  </span>
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
+        <CalendarHeader />
+        <CalendarBody currentDate={today} dateList={dateList}/>
       </table>
     </>
   )
---
+++ src/libs/date.ts
@@ -0,0 +1,8 @@
+import { isSameMonth, isToday } from "date-fns"
+
+export const dateColor = (targetDate: Date, currentDate: Date): string => {
+  if (isToday(targetDate)) return "bg-lime-800 text-white rounded-full";
+  return isSameMonth(targetDate, currentDate)
+    ? "text-black"
+    : "text-gray-300";
+};
