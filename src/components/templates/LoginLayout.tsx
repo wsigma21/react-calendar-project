29deed90b0387dabb815d43e3242e41a61598f97
wsigma21 <wallstlbl@gmail.com>
2024-03-02 10:59:49 +0900

    feat: ログイン後のレイアウトを作成

---
+++ src/components/templates/LoginLayout.tsx
@@ -0,0 +1,42 @@
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { loginUserState } from "../../store/loginUserState";
+import { useRecoilState } from "recoil";
+import { FaUser } from "react-icons/fa";
+import { MdLogout } from "react-icons/md";
+
+export const LoginLayout = () => {
+  const navigate = useNavigate();
+  const [loginUser, setLoginUser] = useRecoilState(loginUserState);
+  const handleLogout = () => {
+    setLoginUser({ id: 0, name: "" })
+    navigate("/login")
+  }
+  if (loginUser.id === 0 ) return <Navigate to="/login" />;
+  return (
+    <div className="realative">
+      <header className="bg-white fixed top-0 left-0 right-0 leading-[50px]">
+        <div className="sm:container sm:mx-auto flex justify-between">
+          <p className="logo">
+            <Link to="/">スケジュール管理APP</Link>
+          </p>
+        </div>
+        <nav>
+          <ul className="flex justify-center gap-5 text-lime-800">
+            <li className="flex items-center">
+              <FaUser />
+              {loginUser.name}
+            </li>
+            <li className="flex items-center">
+              <MdLogout />
+              <a onClick={handleLogout}>ログアウト</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="pt-[50px] bg-gradient-to-r from-lime-100 to-lime-200 h-screen flex flex-col justify-center items-center">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
